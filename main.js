ad3edd0ccea3148e596036ec9145b03cdf49ed51
strjak <strackjuanp@gmail.com>
2025-12-26 04:22:35 +0000

    Rename function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getChoiseFromNumber(num){
+function getChoiceFromNumber(num){
     if (num == 0) return "rock";
     if (num == 1) return "paper";
     if (num == 2) return "scissors";
@@ -8,5 +8,4 @@ function getComputerChoice(){
     const num = Math.floor(Math.random() * 3);
     return getChoiseFromNumber(num);
 }
-
 
