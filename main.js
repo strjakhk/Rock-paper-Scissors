2fa1fc9b2b950dae9459bea9d97adb61abff72ec
strjak <strackjuanp@gmail.com>
2025-12-26 14:35:05 +0000

    Implement playRound and playGame functions

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const items = ["rock", "scissors", "paper"]
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice(){
     const index = Math.floor(Math.random() * 3);
@@ -16,7 +19,7 @@ function getIndexOf(item){
     return index;
 }
 
-function getWinner(humanItem, computerItem){
+function getResult(humanItem, computerItem){
     let hIndex = getIndexOf(humanItem);
     let cIndex = getIndexOf(computerItem);
 
@@ -24,3 +27,52 @@ function getWinner(humanItem, computerItem){
     if (computerItem === items[hIndex + 1]) return 1; // human wins
     else return -1; // computer wins
 }
+
+function tie(item1){
+    console.log("It's a tie! both pick " + item1);
+}
+
+function win(item1, item2){
+    console.log(`You won this one, ${item1} beats ${item2}`);
+    humanScore += 1;
+}
+
+function lose(item1, item2){
+    console.log(`You lose this round! ${item1} beats ${item2}`);
+    computerScore += 1;
+}
+
+function playRound(hChoice, cChoice){
+    const result = getResult(hChoice, cChoice);
+
+    if (!result){
+        tie(hChoice)
+    }else if (result > 0){
+        win(hChoice, cChoice);
+    }else{
+        lose(cChoice, hChoice);
+    }
+}
+
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        const hChoice = getHumanChoice();
+        const cChoice = getComputerChoice();
+
+        playRound(hChoice, cChoice);
+    }
+
+    if (humanScore > computerScore){
+        console.log("You won the game!");
+    }else if (computerScore > humanScore){
+        console.log("You lost the game :(");
+    }else{
+        console.log("It's a tie");
+    }
+}
+
+playGame();
+
+
+
+
