4f0e2b2a0f814b470445e9174954e470365d098f
strjak <strackjuanp@gmail.com>
2025-12-26 14:04:02 +0000

    Implement function to get a winner

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-let computerScore = 0;
-let humanScore = 0;
-
-function getChoiceFromNumber(num){
-    if (num == 0) return "rock";
-    if (num == 1) return "paper";
-    if (num == 2) return "scissors";
-}
+const items = ["rock", "scissors", "paper"]
 
 function getComputerChoice(){
-    const num = Math.floor(Math.random() * 3);
-    return getChoiseFromNumber(num);
+    const index = Math.floor(Math.random() * 3);
+    return items[index];
 }
 
 function getHumanChoice(){
-    const choice = prompt("Rock, paper or scissors?");
+    const choice = prompt("Rock, paper or scissors?").toLowerCase();
     return choice;
+}
+
+function getIndexOf(item){
+    let index = items.indexOf(item);
+    index = (index === items.length - 1) ? -1 : index;
+    return index;
+}
+
+function getWinner(humanItem, computerItem){
+    let hIndex = getIndexOf(humanItem);
+    let cIndex = getIndexOf(computerItem);
+
+    if (hIndex === cIndex) return 0; // tie
+    if (computerItem === items[hIndex + 1]) return 1; // human wins
+    else return -1; // computer wins
 }
