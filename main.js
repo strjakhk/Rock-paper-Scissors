580a4c130cc552d9cd7b40898b163c7cc6e0e015
strjak <strackjuanp@gmail.com>
2026-01-05 05:06:12 +0000

    Implement UI messages for rounds and game over

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 /** @type {NodeListOf<HTMLButtonElement>} */
 const buttons = document.querySelectorAll(".selection > button");
+const result = document.querySelector(".result");
+const roundMsg = document.querySelector(".round-msg");
 
 const items = ["rock", "scissors", "paper"]
 let humanScore = 0;
@@ -7,23 +9,30 @@ let computerScore = 0;
 
 
 // setting up event listeners for each selection button
-buttons.forEach((btn) => {
+buttons.forEach(btn => {
     const choice = btn.id;
     btn.addEventListener("click", () => {
         const computerChoice = getComputerChoice();
         playRound(choice, computerChoice);
+        updateGameStatus();
     })
 });
 
-function getComputerChoice(){
-    const index = Math.floor(Math.random() * 3);
-    return items[index];
+// manage the game over msg and prevent user for continue playing
+function gameOver(winner){
+    const gameOverMsg = document.createElement("div");
+    gameOverMsg.textContent = `GameOver! ${winner} won the game.`;
+    result.appendChild(gameOverMsg);
+    buttons.forEach(btn => btn.setAttribute("disabled", "disabled"));
 }
 
-function getIndexOf(item){
-    let index = items.indexOf(item);
-    index = (index === items.length - 1) ? -1 : index;
-    return index;
+//  update the score points in UI and check if the game is over
+function updateGameStatus(){
+    const score = document.querySelector(".score");
+    score.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
+
+    if (humanScore == 5) gameOver("human");
+    if (computerScore == 5) gameOver("computer");
 }
 
 /* 
@@ -40,20 +49,37 @@ function getResult(humanItem, computerItem){
     else return -1; // computer wins
 }
 
-function tie(item1){
-    console.log("It's a tie! both pick " + item1);
+function getComputerChoice(){
+    const index = Math.floor(Math.random() * 3);
+    return items[index];
 }
 
+function getIndexOf(item){
+    let index = items.indexOf(item);
+    index = (index === items.length - 1) ? -1 : index;
+    return index;
+}
+
+// print a tie message in round
+function tie(item1){
+    roundMsg.textContent = `Both chose ${item1}, so it's a tie.`;
+    result.appendChild(roundMsg);
+}
+
+// prints win message and gives one point to user
 function win(item1, item2){
-    console.log(`You won this one, ${item1} beats ${item2}`);
+    roundMsg.textContent = `You win! ${item1} beats ${item2}!`;
     humanScore += 1;
 }
 
+// prints lose message and gives one point to computer
 function lose(item1, item2){
-    console.log(`You lose this round! ${item1} beats ${item2}`);
+    roundMsg.textContent = `You lose. ${item1} beats ${item2}`;
     computerScore += 1;
 }
 
+
+// manage round logic
 function playRound(hChoice, cChoice){
     const result = getResult(hChoice, cChoice);
 
